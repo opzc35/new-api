2cc45756b8111ded9efc23bf14f32c392b7a636d
opzc35 <opzc35@gmail.com>
2025-12-28 14:17:08 +0000

    修复了一系列漏洞，现在可以在充值或者兑换兑换码之后升级为vip分组了

--- model/upgradeuser.go
+++ model/upgradeuser.go
@@ -8,7 +8,7 @@ import (
 
 // UpdateUserGroup 将用户的 group 字段更新为指定值，并更新缓存（如果有缓存逻辑）
 func UpdateUserGroup(userId int, newGroup string) error {
-    if err := DB.Model(&User{}).Where("id = ?", userId).Update("`group`", newGroup).Error; err != nil {
+    if err := DB.Model(&User{}).Where("id = ?", userId).Updates(map[string]interface{}{"group": newGroup}).Error; err != nil {
         common.SysLog("failed to update user group: " + err.Error())
         return err
     }
